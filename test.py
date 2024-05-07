b020c39217155244b77a26c36d6e5d1f8f7095f9
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-07 13:49:35 +0530

    Added a loop

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 print("Welcome to Ubuntu\nI am gonna test GIT here")
 print("Here I am gonna Test Git and Github")
+
+print("This code is belongs to Loops branch")
+for i in range(10):
+    print(i, "Sujahath")
